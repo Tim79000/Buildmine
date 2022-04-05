b813dd26646430140b6da9cee1416f37ad2e3b89
Tim7900 <100736284+Tim79000@users.noreply.github.com>
2022-04-04 17:00:54 -0800

    :|
    :|

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import pickle
 from ursina import *
 
 app = Ursina()
 
+plums = ["1"]
+
+print ('Buildmine 0.0.1')
 player = Sprite('player.png')
+cloud = Sprite('cloud.png')
 def update():
     player.y += held_keys['w'] * time.dt
     player.x -= held_keys['a'] * time.dt
     player.y -= held_keys['s'] * time.dt
     player.x += held_keys['d'] * time.dt
+    cloud.x += time.dt
 
-
+'''
+Hello MadKamel. :)
+'''
 
 app.run()
